47793f35e8d76c31d54c9e20131aecbe6efdf0e3
Greg Keeney <gkeeney29@gmail.com>
2015-09-18 12:11:18 -0400

    Roll

---
+++ Roll a Ball/Assets/Scripts/Flip.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Flip : MonoBehaviour {
+    public Text countText;
+
+    void Update()
+    {
+        if (countText.text =="Count: 12")
+        {
+            transform.Rotate(new Vector2(90, 0) * Time.deltaTime);
+        }
+    }
+}
---
+++ Roll a Ball/Assets/Scripts/Flip2.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Flip2 : MonoBehaviour
+{
+    public Text countText;
+
+    void Update()
+    {
+        if (countText.text == "Count: 12")
+        {
+            transform.Rotate(new Vector3(90,0 ,90) * Time.deltaTime);
+        }
+    }
+}
